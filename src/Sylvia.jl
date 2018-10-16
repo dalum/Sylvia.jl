37f394c39be3660f7d86b17270661f4534b16b44
Sakse Dalum <dalum@users.noreply.github.com>
2018-10-16 18:34:16 +0800

    Complete overhaul for v1.0

--- src/Sylvia.jl
+++ src/Sylvia.jl
@@ -1,264 +1,310 @@
-__precompile__(true)
-
 module Sylvia
 
-export @S_str, @def, @λ, @symbols, @symbols!, @symbols!!
+export @S_str, @rule, @symbols, @λ
 
-const Properties = Dict{Symbol,Any}
+const BASE_FUNCTION_SYMBOLS =
+    (:+, :-, :!, :exp, :log, :sqrt, :inv,
+     :cos, :sin, :tan, :sec, :csc, :cot, :cosh, :sinh, :tanh, :sech, :csch, :coth,
+     :acos, :asin, :atan, :asec, :acsc, :acot, :acosh, :asinh, :atanh, :asech, :acsch, :acoth)
 
-struct Symbolic{T, C}
-    value::T
-    #properties::Properties
-    Symbolic{T, C}(value, properties) where {T, C} = new(value) #new(value, properties)
+const SymbolOrExpr = Union{Symbol,Expr}
+
+struct Symbolic{T,HASH}
+    data::T
+end
+Symbolic{T}(data) where {T} = Symbolic{T,hash(data)}(data)
+Symbolic(data::T) where {T} = Symbolic{T,hash(data)}(data)
+Symbolic(data::T, h::UInt) where {T} = Symbolic{T,h}(data)
+
+macro S_str(str::String)
+    Symbolic(Meta.parse(str))
 end
 
-const Namespace = Dict{Symbol, Symbolic{Symbol}}
-const GLOBAL_NAMESPACE = Namespace()
-
-DEFAULT_SYMBOL_TYPE = Number
-DEFAULT_EXPR_TYPE = Any
-
-"""
-    Symbolic([C,] value[, properties])
-"""
-Symbolic(::Type{C}, value, properties) where {C} = Symbolic{typeof(value), C}(value, properties)
-Symbolic(::Type{C}, value) where {C} = Symbolic(C, value, Properties())
-Symbolic(value) = Symbolic(value, Properties())
-Symbolic(value, properties::Properties) = Symbolic(typeof(value), value, properties)
-Symbolic(value::Symbol, properties::Properties) = Symbolic(DEFAULT_SYMBOL_TYPE, value, properties)
-Symbolic(value::Expr, properties::Properties) = Symbolic(DEFAULT_EXPR_TYPE, value, properties)
-
-"""
-    symbol([C,] x; namespace=GLOBAL_NAMESPACE, safe=true)
-"""
-symbol(x) = Symbolic(x)
-symbol(x::Symbolic) = x
-symbol(expr::Expr) = replacesymbols(Val{expr.head}(), expr)
-
-function symbol(::Type{C}, x::Symbol, properties::Properties; namespace::Namespace = GLOBAL_NAMESPACE, safe::Bool = true) where C
-    if safe && x in keys(namespace)
-        return namespace[x]
+macro symbols(xs...)
+    ret = Expr(:block)
+    tup = Expr(:tuple)
+    for x in xs
+        @assert x isa Symbol
+        push!(ret.args, Expr(:(=), esc(x), :(Symbolic($(QuoteNode(x))))))
+        push!(tup.args, esc(x))
     end
-    return namespace[x] = Symbolic{typeof(x), C}(x, properties)
-end
-symbol(x::Symbol; kw...) = symbol(DEFAULT_SYMBOL_TYPE, x; kw...)
-symbol(::Type{C}, x::Symbol; kw...) where {C} = symbol(C, x, Properties(); kw...)
-symbol(x::Symbol, properties::Properties; kw...) = symbol(DEFAULT_SYMBOL_TYPE, x, properties; kw...)
-
-
-macro S_str(str)
-    symbol(parse(str))
+    push!(ret.args, tup)
+    return ret
 end
 
-"""
-    @symbols
-"""
-macro symbols(exprs...)
-    _symbols(exprs...; safe=true)
-end
+Base.show(io::IO, x::Symbolic) = print(io, "$(showify(x))")
+Base.hash(::Symbolic{T,HASH}) where {T,HASH} = HASH
 
-macro symbols!(exprs...)
-    _symbols(exprs...; safe=false)
-end
-
-macro symbols!!(exprs...)
-    empty!(GLOBAL_NAMESPACE)
-    _symbols(exprs...; safe=false)
-end
-
-function _symbols(exprs...; namespace::Symbol = :GLOBAL_NAMESPACE, safe::Bool = true)
-    T = DEFAULT_SYMBOL_TYPE
-    properties = :(Properties())
-    retexpr = Expr(:block)
-    for expr in exprs
-        if expr isa Symbol
-            push!(retexpr.args, Expr(:(=), esc(expr), :(symbol($T, $(Expr(:quote, expr)), $properties; namespace=$namespace, safe=$safe))))
-        elseif expr isa Expr && expr.head === :vect && length(expr.args) >= 1
-            T = esc(expr.args[1])
-            properties = Expr(:call, :Properties, expr.args[2:end]...)
-        else
-            error("invalid type annotation or symbol: $expr")
-        end
-    end
-    push!(retexpr.args, esc(:nothing))
-    return retexpr
-end
-
-SymbolOrExpr{T} = Union{Symbolic{Symbol,T}, Symbolic{Expr,T}}
-Class{T} = Symbolic{<:Any,T}
-
-### Value extraction
 value(x) = x
-value(x::Symbolic) = x.value
-properties(x::Symbolic) = x.properties
+value(x::Symbolic) = x.data
 
-### Promotion rules
-Base.convert(::Type{Any}, x::Symbolic) = x
-Base.convert(::Type{Symbolic}, x) = Symbolic(x)
-Base.convert(::Type{Symbolic{T,C} where T}, x) where {C} = Symbolic(C, x)
-Base.convert(::Type{Symbolic{T,C} where T}, x::Symbolic{T2,C}) where {T2,C} = Symbolic(C, x.value)
-Base.convert(::Type{Symbolic{T,C}}, x) where {T,C} = Symbolic(C, convert(T, x))
-Base.convert(::Type{Symbolic}, x::Symbolic{T,C} where {T,C}) = x
-Base.convert(::Type{Symbolic{T}}, x::Symbolic{T,C}) where {T,C} = x
-Base.convert(::Type{Symbolic{T,C1}}, x::Symbolic{T,C2}) where {T,C1,C2} = Symbolic(C1, x.value)
-Base.convert(::Type{Symbolic{T1}}, x::Symbolic{T2,C}) where {T1,T2,C} = Symbolic(C, convert(T1, x.value))
-Base.convert(::Type{Symbolic{T1,C}}, x::Symbolic) where {T1,C} = Symbolic(C, convert(T1, x.value))
-Base.convert(::Type{T1}, x::Symbolic{T2}) where {T1,T2} = convert(T1, x.value)
+getsymbols(x::Symbolic) = getsymbols(value(x))
+getsymbols(::Number) = Set(Symbol[])
+getsymbols(x::Symbol) = Set(Symbol[x])
+getsymbols(A::AbstractArray) = mapreduce(getsymbols, union, A)
+getsymbols(x::Expr) = getsymbols(Val{x.head}, x.args)
+getsymbols(::Type{<:Val}, args) = mapreduce(getsymbols, union, args)
+getsymbols(::Type{Val{:call}}, args) = mapreduce(getsymbols, union, args[2:end])
 
-Base.convert(::Type{AbstractArray{Symbolic{T1},N}}, A::AbstractArray{T2,N}) where {T1<:Number,T2,N} = Base.convert(AbstractArray{Symbolic,N}, A)
-
-Base.promote_rule(::Type{Symbolic}, ::Type{<:Any}) = Symbolic
-
-show_prefix = ""
-show_suffix = ""
-function setshow(prefix, suffix)
-    global show_prefix, show_suffix
-    show_prefix = prefix
-    show_suffix = suffix
-    println("Symbolic types printed as print(Symbolic(x)) -> $(show_prefix)x$(show_suffix)")
-end
-
-Base.show(io::IO, x::Symbolic) = print(io, "$show_prefix$(expressify(x))$show_suffix")
-
-include("expression.jl")
-
-#="""
-    iscommutative(f, x, y)
-"""=#
-iscommutative(::Function, ::Type, ::Type) = false
-iscommutative(f::Function, x1::Class{C1}, x2::Class{C2}) where {C1, C2} = x1 == x2 || iscommutative(f, C1, C2)
-iscommutative(f::Function, ::Type{<:Class{C1}}, ::Type{<:Class{C2}}) where {C1, C2} = iscommutative(f, C1, C2)
-
-macro commutes(f, T, S)
-    :(iscommutative(::typeof($f), ::Type{<:$T}, ::Type{<:$S}) = true)
-end
-@commutes(+, Number, Number)
-@commutes(+, Number, AbstractArray)
-@commutes(+, AbstractArray, Number)
-@commutes(+, AbstractArray, AbstractArray)
-
-@commutes(*, Number, Number)
-@commutes(*, Number, AbstractArray)
-@commutes(*, AbstractArray, Number)
-
-include("derive.jl")
-include("math.jl")
-include("simplify.jl")
-
-macro _call(verbose, name, symbols, expr)
-    if eval(verbose)
-        return esc(quote
-                   print("$($name)(", join($symbols, ", "), ")")
-                   retval = $expr
-                   println(" => $retval")
-                   retval
-                   end)
-    else
-        return esc(expr)
-    end
-end
-
-function define_operators(verbose::Bool)
-
-    ### Undressed return values
-    for op in (:(Base.:(==)), :(Base.:<))
-        @eval $op(x::Symbolic, y) = @_call $verbose $op (x, y) $op(x, Symbolic(y))
-        @eval $op(x, y::Symbolic) = @_call $verbose $op (x, y) $op(Symbolic(x), y)
-    end
-
-    ### Dressed return values
-    for op in (:(Base.:+), :(Base.:-), :(Base.:*), :(Base.:/), :(Base.:\), :(Base.://), :(Base.:^))
-        @eval $op(x::Symbolic, y)::Symbolic = @_call $verbose $op (x, y) $op(x, Symbolic(y))
-        @eval $op(x, y::Symbolic)::Symbolic = @_call $verbose $op (x, y) $op(Symbolic(x), y)
-    end
-
-    # Needs special treatment
-    Base.:^(x::Symbolic, y::Integer)::Symbolic = x ^ Symbolic(y)
-
-end
-
-define_operators(false)
-
-"""
-    debug(value)
-"""
-function debug(value::Bool = true)
-    if value
-        setshow("S\"", "\"")
-        define_operators(true)
-    else
-        setshow("", "")
-        define_operators(false)
-    end
-    print("Debug: $value")
-end
-
-#="""
-    expressify(a)
-"""=#
 expressify(a) = a # Catch all
 expressify(a::Expr) = Expr(a.head, expressify.(a.args)...)
 expressify(a::Symbolic) = expressify(value(a))
-expressify(V::AbstractVector) = Expr(:vect, value.(V)...)
-expressify(A::AbstractMatrix) = Expr(:vcat, mapslices(x -> Expr(:row, value.(x)...), A, 2)...)
+expressify(v::AbstractVector) = Expr(:vect, expressify.(v)...)
+expressify(A::AbstractMatrix) = Expr(:vcat, mapslices(x -> Expr(:row, expressify.(x)...), A, dims=2)...)
 
-# def
+showify(a) = a # Catch all
+showify(a::Expr) = Expr(a.head, showify.(a.args)...)
+showify(a::Symbolic) = showify(value(a))
 
-_def(mod::Module, expr::Symbol) = throw(ArgumentError("invalid function definition"))
-function _def(mod::Module, expr::Expr)
-    if expr.head ≠ :(=)
-        throw(ArgumentError("invalid function definition"))
-    end
-
-    body = expressify(eval(mod, expr.args[2]))
-
-    name = expr.args[1]
-    if name isa Symbol
-        symbols = sort(collect(getsymbols(body)))
-    elseif iscall(name)
-        name, symbols = name.args[1], name.args[2:end]
-    else
-        throw(ArgumentError("name must be either a symbol or call"))
-    end
-    return esc(Expr(:(=), Expr(:call, name, symbols...), body))
-end
-
-if VERSION >= v"0.7.0-DEV"
-    macro def(expr) _def(__module__, expr) end
-else
-    macro def(expr) _def(Main, expr) end
-end
-macro def(mod, expr) _def(mod, expr) end
-
-# λ
-
-_parse_args(args::Symbol) = (args,)
-function _parse_args(args::Expr)
-    if args.head ≠ :tuple
-        throw(ArgumentError("invalid argument list"))
-    end
-    return args.args
-end
-
-function _lambda_implicit(mod::Module, expr)
-    body = expressify(eval(mod, expr))
+macro λ(expr)
+    body = expressify(Base.eval(__module__, expr))
     symbols = sort(collect(getsymbols(body)))
-    esc(Expr(:(->), Expr(:tuple, symbols...), body))
+    return esc(Expr(:(->), Expr(:tuple, symbols...), body))
 end
 
-_lambda(mod::Module, x::Symbol) = _lambda_implicit(mod, x)
-function _lambda(mod::Module, expr::Expr)
-    expr.head ≠ :(->) && return _lambda_implicit(mod, expr)
-    symbols = _parse_args(expr.args[1])
-    body = expressify(eval(mod, expr.args[2]))
-    esc(Expr(:(->), Expr(:tuple, symbols...), body))
+##################################################
+# Promotion/conversion
+##################################################
+
+Base.convert(::Type{Symbolic}, a::Symbolic) = a
+Base.convert(::Type{Symbolic{T}}, a::Symbolic) where {T} = Symbolic{T}(value(a))
+Base.convert(::Type{Symbolic{T,HASH}}, a::Symbolic) where {T,HASH} = Symbolic{T,HASH}(value(a))
+
+Base.convert(::Type{Symbolic}, a) = Symbolic(a)
+Base.convert(::Type{Symbolic{T}}, a) where {T} = Symbolic{T}(a)
+Base.convert(::Type{Symbolic{T,HASH}}, a) where {T,HASH} = Symbolic{T,HASH}(a)
+
+Base.convert(::Type{AbstractArray{Symbolic{T,HASH}}}, A::AbstractArray) where {T,HASH} = Base.convert(AbstractArray{Symbolic}, A)
+
+Base.promote_rule(::Type{Symbolic{T,HASH}}, ::Type{Any}) where {T,HASH} = Symbolic
+function Base.promote_rule(::Type{Symbolic{T,HASH}}, ::Type{S}) where {T,HASH,S}
+    Q = promote_type(T,S)
+    return isconcretetype(Q) ? Symbolic{Q} : Symbolic
 end
 
-if VERSION >= v"0.7.0-DEV"
-    macro λ(expr) _lambda(__module__, expr) end
-else
-    macro λ(expr) _lambda(Main, expr) end
+##################################################
+# Rules and assumptions
+##################################################
+
+macro rule(expr::Expr)
+    return rule(expr)
 end
-macro λ(mod, expr) _lambda(mod, expr) end
+
+function rule(expr)
+    head, args = expr.head, expr.args
+    @assert head === :(-->)
+    lhs, rhs = args
+    @assert lhs isa Expr && lhs.head === :call
+    MODULE_= @__MODULE__
+    unique_symbols = Set()
+    new_lhs = Expr(:where, lhs)
+    fnsymbol = lhs.args[1]
+    if lhs.args[1] in BASE_FUNCTION_SYMBOLS
+        fnsymbol = Expr(:., :Base, QuoteNode(fnsymbol))
+    end
+    lhs.args[1] = esc(fnsymbol)
+    for i in 2:length(lhs.args)
+        if lhs.args[i] isa Expr && lhs.args[i].head === :(...)
+            symbol = Symbol(lhs.args[i].args[1])
+            T = Symbol("T#$symbol")
+            lhs.args[i].args[1] = Expr(:(::), esc(Symbol(symbol, "s")), T)
+        elseif lhs.args[i] isa Symbol || lhs.args[i] isa Number
+            symbol = lhs.args[i]
+            T = Symbol("T#$symbol")
+            if !(symbol in unique_symbols)
+                if symbol isa Number
+                    lhs.args[i] = Expr(:(::), T)
+                    push!(new_lhs.args, Expr(:(<:), T, Expr(:call, :_typeof, Expr(:call, :Symbolic, esc(symbol)))))
+                else
+                    lhs.args[i] = Expr(:(::), esc(symbol), T)
+                    push!(new_lhs.args, Expr(:(<:), T, Expr(:call, :_typeof, esc(symbol))))
+                end
+                push!(unique_symbols, symbol)
+            else
+                lhs.args[i] = Expr(:(::), T)
+            end
+        end
+    end
+    unique!(new_lhs.args)
+    return Expr(:(=), new_lhs, Expr(:block, Expr(:meta, :inline), esc(rhs)))
+end
+
+@inline _typeof(x) = Base.typeof(x)
+
+##################################################
+# Expression manipulations
+##################################################
+
+split(op, x::Symbolic)::Vector{Symbolic} = [x]
+function split(op, x::Symbolic{Expr})::Vector{Symbolic}
+    val = value(x)
+    if val.head == :call && val.args[1] == op
+        return val.args[2:end]
+    end
+    return [x]
+end
+
+join(op, xs) = join(op, xs, hash((hash(eval(:(Base.$op))), hash.(xs)...)))
+function join(op, xs, h)
+    return Symbolic(Expr(:call, op, xs...), h)
+end
+
+##################################################
+# Wildcards
+##################################################
+
+const Wildcard{T} = Symbolic{T,UInt(0)}
+Wildcard() = Wildcard{Any}(Symbol("#Any#"))
+Wildcard{Symbol}() = Wildcard{Symbol}(Symbol("#Symbol#"))
+Wildcard{SymbolOrExpr}() = Wildcard{SymbolOrExpr}(Symbol("#SymbolOrExpr#"))
+Wildcard{T}() where {T<:Number} = Wildcard{T}(one(T))
+
+showify(::Wildcard{T}) where {T<:Number} = Symbol("#$T#")
+
+@inline _typeof(::Wildcard{T}) where {T} = Symbolic{<:T}
+
+##################################################
+# Base
+##################################################
+
+# One-arg operators
+for op in (:+, :-, :!, :exp, :log, :sqrt, :inv,
+           :cos, :sin, :tan, :sec, :csc, :cot, :cosh, :sinh, :tanh, :sech, :csch, :coth,
+           :acos, :asin, :atan, :asec, :acsc, :acot, :acosh, :asinh, :atanh, :asech, :acsch, :acoth)
+    name, opsymbol = :(Base.$op), QuoteNode(op)
+    @eval function $name(a::Symbolic)
+        return join($opsymbol, (a,))
+    end
+    @eval function $name(a::Wildcard)
+        return join($opsymbol, (a,))
+    end
+end
+
+# Two-arg operators
+for op in (:-, :/, :\, ://, :^, :÷, :(==), :&, :|)
+    name, opsymbol = :(Base.$op), QuoteNode(op)
+    @eval $name(a::Symbolic, b) = $name(promote(a, b)...)
+    @eval $name(a, b::Symbolic) = $name(promote(a, b)...)
+    @eval function $name(a::Symbolic, b::Symbolic)
+        return join($opsymbol, (a, b))
+    end
+    @eval function $name(a::Wildcard, b::Wildcard)
+        return join($opsymbol, (a, b))
+    end
+end
+
+# Multi-arg operators
+for op in (:+, :*)
+    name, opsymbol = :(Base.$op), QuoteNode(op)
+    @eval $name(a::Symbolic, b) = $name(promote(a, b)...)
+    @eval $name(a, b::Symbolic) = $name(promote(a, b)...)
+    @eval function $name(a::Symbolic, b::Symbolic)
+        xs = vcat(split($opsymbol, a), split($opsymbol, b))
+        return join($opsymbol, xs)
+    end
+    @eval function $name(a::Wildcard, b::Wildcard)
+        xs = vcat(split($opsymbol, a), split($opsymbol, b))
+        return join($opsymbol, xs)
+    end
+end
+
+##################################################
+# Zeros and ones
+##################################################
+
+const Zero{T} = Symbolic{T,hash(0)}
+Zero{Symbol}(x) = Zero{Bool}(x)
+Zero{Expr}(x) = Zero{Bool}(x)
+Zero() = Zero{Bool}(0)
+Zero(T) = Zero{T}(0)
+Zero(T, S) = Zero{promote_type(T, S)}(0)
+Zero(T, S, Qs...) = Zero{promote_type(T, S, Qs...)}(0)
+
+const One{T} = Symbolic{T,hash(1)}
+One{Symbol}(x) = One{Bool}(x)
+One{Expr}(x) = One{Bool}(x)
+One() = One{Bool}(1)
+One(T) = One{T}(1)
+One(T, S) = One{promote_type(T, S)}(1)
+One(T, S, Qs...) = One{promote_type(T, S, Qs...)}(1)
+
+showify(::Zero) = Symbol("0")
+showify(::One) = Symbol("1")
+
+@inline Base.zero(::Symbolic) = Zero()
+@inline Base.zero(::Symbolic{T}) where {T<:Number} = Zero{T}()
+@inline Base.zero(::Type{<:Symbolic}) = Zero()
+@inline Base.zero(::Type{<:Symbolic{T}}) where {T<:Number} = Zero{T}()
+
+@inline Base.one(::Symbolic) = One()
+@inline Base.one(::Symbolic{T}) where {T<:Number} = One{T}()
+@inline Base.one(::Type{<:Symbolic}) = One()
+@inline Base.one(::Type{<:Symbolic{T}}) where {T<:Number} = One{T}()
+
+@inline Base.oneunit(::Symbolic) = One()
+@inline Base.oneunit(::Symbolic{T}) where {T<:Number} = One{T}()
+@inline Base.oneunit(::Type{<:Symbolic}) = One()
+@inline Base.oneunit(::Type{<:Symbolic{T}}) where {T<:Number} = One{T}()
+
+@inline Base.iszero(::Symbolic) = false
+@inline Base.iszero(x::Symbolic{<:Number}) = iszero(value(x))
+@inline Base.iszero(::Zero) = true
+@inline Base.iszero(::One) = false
+
+@inline Base.isone(::Symbolic) = false
+@inline Base.isone(x::Symbolic{<:Number}) = isone(value(x))
+@inline Base.isone(::Zero) = false
+@inline Base.isone(::One) = true
+
+@inline Base.isinf(::Symbolic) = false
+@inline Base.isinf(x::Symbolic{<:Number}) = isinf(value(x))
+
+##################################################
+# Default rules
+##################################################
+
+const x = Wildcard{SymbolOrExpr}(:x)
+
+@rule Base.:(==)(::Zero, ::Zero) --> true
+@rule Base.:(==)(::One, ::One) --> true
+@rule Base.:(==)(::Zero, ::One) --> false
+@rule Base.:(==)(::One, ::Zero) --> false
+
+@rule Base.:-(x, ::Zero) --> x
+@rule Base.:*(::One, x) --> x
+@rule Base.:*(x, ::One) --> x
+@rule Base.:\(::One, x) --> x
+@rule Base.:/(x, ::One) --> x
+
+@rule Base.:-(::Zero, x) --> -x
+
+@rule Base.:-(x, x) --> zero(x)
+@rule Base.:*(::Zero, x) --> zero(x)
+@rule Base.:*(x, ::Zero) --> zero(x)
+
+@rule Base.:+(x, x) --> 2x
+@rule Base.:+(x, x, x...) --> (2 + length(xs))*x
+
+@rule Base.:*(x, x) --> x^2
+@rule Base.:*(x, x, x...) --> x^(2 + length(xs))
+
+@inline Base.:+(::Zero{T}, ::Zero{S}) where {T,S} = Zero(T, S)
+@inline Base.:-(::Zero{T}, ::Zero{S}) where {T,S} = Zero(T, S)
+@inline Base.:*(::Zero{T}, ::Zero{S}) where {T,S} = Zero(T, S)
+
+@inline Base.:-(::One{T}, ::One{S}) where {T,S} = Zero(T, S)
+@inline Base.:*(::One{T}, ::One{S}) where {T,S} = One(T, S)
+@inline Base.:/(::One{T}, ::One{S}) where {T,S} = One(T, S)
+@inline Base.:\(::One{T}, ::One{S}) where {T,S} = One(T, S)
+
+@inline Base.:-(::Zero{T}, ::One{S}) where {T,S} = One(T, S)
+@inline Base.:-(::One{T}, ::Zero{S}) where {T,S} = One(T, S)
+@inline Base.:*(::Zero{T}, ::One{S}) where {T,S} = Zero(T,S)
+@inline Base.:*(::One{T}, ::Zero{S}) where {T,S} = Zero(T,S)
+@inline Base.:/(::Zero{T}, ::One{S}) where {T,S} = Zero(T,S)
+@inline Base.:\(::One{T}, ::Zero{S}) where {T,S} = Zero(T,S)
+
+@inline Base.:/(a::T, b::T) where {T<:Symbolic} = (!iszero(a) && !isinf(b)) ? one(a) : join(:/, (a, b))
+@inline Base.:\(a::T, b::T) where {T<:Symbolic} = (!iszero(b) && !isinf(a)) ? one(b) : join(:\, (a, b))
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,111 +2,27 @@ using Base.Test
 using Sylvia
 using Sylvia: Symbolic, symbol
 
-@symbols a b c d [Matrix{Number}] A B C D
+@symbols a b c d
 
 # Conversion
 @test symbol(a) == a == :a
 @test [a] == Symbolic[:a]
 @test convert(Int, Symbolic(1)) === Int(1)
-@test convert(Symbolic{AbstractFloat,typeof(1.0)}, Symbolic(1)) === Symbolic(1.0)
+@test convert(Symbolic{AbstractFloat}, Symbolic(1)) === Symbolic(1.0)
 @test iszero(Symbolic(0)) & iszero(Symbolic(0.0))
-@test Sylvia.isone(Symbolic(1)) & Sylvia.isone(Symbolic(1.0))
+@test isone(Symbolic(1)) & isone(Symbolic(1.0))
 
 # Basic identities
 @test a + 0 == a
-@test a + S"1 - 1" == a
-@test a + b - a == b
+@test a - a == 0
 @test a - 0 == a
 @test a * 1 == a
 @test a / 1 == a
-
-@test a - a == 0
+@test 1 \ a == a
 @test a * 0 == 0
 @test 0 / a == 0
 @test a \ 0 == 0
-@test 1 ^ a == 1
-
-@test a / 0 == Inf * a
-@test -a / 0 == -Inf * a
+@test a ^ 1 == a
 
 @test a + a == 2a
-@test a * 2 == 2a
-
-@test a ^ 2 == a * a
-@test a ^ -1 == 1 / a
-@test a ^ -1 == a \ 1
-
-@test a * b == b * a
-@test A * B != B * A
-
-@test -(a - b) == b - a
-@test a + b + c == a + c + b == b + a + c == b + c + a == c + b + a == c + a + b
-@test a + b - c == a - c + b == -c + a + b
-@test (a + b) - (c + d) == a + b - c - d
-@test a + (b - c) == (a + b) - c
-@test a + 1 == 1 + a
-@test a + 1 + 2 == 3 + a
-@test -1 + a == a - 1
-@test a * 2 == a * 1 * 2 == 2 * a
-
-@test a * (a * b) == (a * a) * b
-@test a * (b * c) * c == (a * b) * (c * c)
-
-# @test 1 / (a / b) == b / a
-# @test a / b \ a == b
-# @test a / -b \ a == -b
-@test a * b^-1 == a / b
-@test a / b^-1 == a * b
-
-# Conjugation and transpose
-
-# @test conj(1im * a) == -1im * conj(a)
-# @test (1im * a)' == -1im * a'
-# @test (1im * a).' == 1im * a.'
-# @test a' != a.'
-# @test conj(a) != a'
-
-# Simplify
-
-@test a * c + b * c == Sylvia.expand((a + b) * c)
-@test a * c + a * b == Sylvia.expand(a * (b + c))
-
-# Arrays
-
-M = [a b; c d]
-
-@test M.' == Symbolic[a.' c.'; b.' d.']
-@test M'  == Symbolic[a' c'; b' d']
-@test M^2 == Symbolic[a ^ 2 + b * c  a * b + b * d
-                      c * a + d * c  c * b + d ^ 2]
-@test M^-1 == Symbolic[(1 + (b * c) / (a * ((-b * c) / a + d))) / a   -b / (a * ((-b * c) / a + d))
-                       -c / (a * ((-b * c) / a + d))                  ((-b * c) / a + d) ^ -1]
-
-#@test Symbolic([M 0I]) == Symbolic[ a b 0 0; c d 0 0 ]
-
-# Functions
-
-@def f = a - b
-@def g(b, a) = a - b
-
-@test f(4, 3) == -g(4, 3) == @λ(a-b)(4, 3) == 1
-
-# @test_throws ArgumentError eval(@macroexpand @def f)
-# @test_throws ArgumentError eval(@macroexpand @def f -> a + b)
-# @test_throws ArgumentError eval(@macroexpand @def 4 = 4)
-# @test_throws ArgumentError eval(@macroexpand @λ [a, b] -> a + b)
-
-# # Symbols
-
-# @test Sylvia.getsymbols(0) == Set([])
-# @test Sylvia.getsymbols(a) == Set(Symbol[a])
-# @test Sylvia.getsymbols(a') == Set(Symbol[a])
-# @test Sylvia.getsymbols(a + b) == Set(Symbol[a, b])
-# @test Sylvia.getsymbols([a b; b a]) == Set(Symbol[a, b])
-
-# @test Sylvia.hassymbols(0) == Sylvia.hassymbols(S"1 + 2") == Sylvia.hassymbols(S"f(1)")
-
-# # debug mode
-
-# Sylvia.debug()
-# @test string(a * b) == "S\"a * b\""
+@test a * a == a^2
