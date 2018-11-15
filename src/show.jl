4062537fa07743f46e0f87ce830dba2b8537c772
Sakse Dalum <dalum@users.noreply.github.com>
2018-11-16 01:58:12 +0800

    Make show methods more faithful

--- src/show.jl
+++ src/show.jl
@@ -2,7 +2,16 @@ const ANNOTATE = Ref(false)
 
 function Base.show(io::IO, x::T) where {T <: Sym}
     e = expr(x, annotate=ANNOTATE[])
+    if !get(io, :compact, false)
+        print(io, "@! ")
+    end
     print(io, sprint(print, e, context=io))
+end
+
+function Base.show_unquoted(io::IO, x::T, ::Int, precedence::Int) where {T <: Sym}
+    print(io, "(")
+    show(io, x)
+    print(io, ")")
 end
 
 function set_annotations(yes::Bool)
